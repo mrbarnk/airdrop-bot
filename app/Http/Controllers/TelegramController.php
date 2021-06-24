b84f64762a3733594fd545b275c27957a61f3cc4
Mrbarnk <mrbarnk1@gmail.com>
2021-06-24 21:34:10 +0100

    New update

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -70,7 +70,7 @@ Click \"Join Airdrop\" to proceed\"
             '📘 Submit my details' => "
 \u{1f539} Join our telegram group and channel
 (button)[D O N E]",
-            ':wink: Done' => "\u{1f539} Follow us on Twitter, like and retweet pinned message
+            'D O N E' => "\u{1f539} Follow us on Twitter, like and retweet pinned message
 
             Submit your Twitter profile link (Example: https://www.twitter.com/yourusername)',
             
